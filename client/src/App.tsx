8852d9072286c97db2a4a42b463842cb9cb76b46
asc-it-minds <asc@it-minds.dk>
2023-03-09 09:25:51 +1100

    Docker setup

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,23 +1,23 @@
-import React from 'react';
+import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import LoginPage from './pages/LoginPage/LoginPage';
-import MainPage from './pages/MainPage/MainPage';
-import RegisterPage from './pages/RegisterPage/RegisterPage';
-
+import Navbar from "./components/Navbar/Navbar";
+import LoginPage from "./pages/LoginPage/LoginPage";
+import MainPage from "./pages/MainPage/MainPage";
+import RegisterPage from "./pages/RegisterPage/RegisterPage";
 
 const App = () => {
-  return (
-    <div className="App">
-       <BrowserRouter>
-            <Routes>
-              <Route path="/" element={<MainPage />} />
-              <Route path="/login" element={<LoginPage />} />
-              <Route path="/register" element={<RegisterPage />} />
-            </Routes>
-          
-    </BrowserRouter>
-    </div>
-  );
-}
+    return (
+        <div className="App">
+            <BrowserRouter>
+            <Navbar />
+                <Routes>
+                    <Route path="/" element={<MainPage />} />
+                    <Route path="/login" element={<LoginPage />} />
+                    <Route path="/register" element={<RegisterPage />} />
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
+};
 
 export default App;
---
+++ client/src/components/Navbar/Navbar.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import { useNavigate } from "react-router-dom";
+
+const Navbar = () => {
+    const navigate = useNavigate()
+
+    return (
+        <div style={{display: "flex"}}>
+            <AppBar position="static">
+                <Toolbar>
+                    <Typography
+                        variant="h6"
+                        component="div"
+                        sx={{ flexGrow: 1 }}
+                        >
+                        Music at RMIT
+                    </Typography>
+                    <Button color="inherit" onClick={() => navigate("/")}>Home</Button>
+                    <Button color="inherit" onClick={() => navigate("/login")}>Login</Button>
+                    <Button color="inherit" onClick={() => navigate("/register")}>Register</Button>
+                </Toolbar>
+            </AppBar>
+        </div>
+    );
+};
+
+export default Navbar;
